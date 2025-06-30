e9c5be93bdd39f3ae815b22305a942980685aeb6
Aldiansyah Fahmi <aldiansyahfahmi6@gmail.com>
2025-06-30 21:18:24 +0800

    single post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,17 +10,46 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     $posts = [
         [
+            'id' => 1,
+            'slug' => 'post-1',
             'title' => 'Post 1',
             'author' => 'Aldiansyah',
             'body' => 'Illo qui qui consectetur vero libero temporibus quam reprehenderit praesentium. Officiis magnam dolorem debitis ut et dolorum ut hic explicabo fuga expedita. Vel adipisci iste quae placeat sit molestiae corporis placeat laudantium. Velit aliquam quia corrupti quisquam dicta et et. Et facilis autem aut consectetur et quia eligendi blanditiis numquam iusto.'
         ],
         [
+            'id' => 2,
+            'slug' => 'post-2',
             'title' => 'Post 2',
             'author' => 'Aldiansyah',
             'body' => 'Doloribus sit autem cupiditate et eos aperiam nobis ullam laboriosam quo ipsum aut dignissimos rem natus. Possimus et nisi doloremque dolor molestias earum. Fugit quam aliquam dolor. Molestiae esse minima tenetur totam sit at molestias et.'
         ]
     ];
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'post-1',
+            'title' => 'Post 1',
+            'author' => 'Aldiansyah',
+            'body' => 'Illo qui qui consectetur vero libero temporibus quam reprehenderit praesentium. Officiis magnam dolorem debitis ut et dolorum ut hic explicabo fuga expedita. Vel adipisci iste quae placeat sit molestiae corporis placeat laudantium. Velit aliquam quia corrupti quisquam dicta et et. Et facilis autem aut consectetur et quia eligendi blanditiis numquam iusto.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'post-2',
+            'title' => 'Post 2',
+            'author' => 'Aldiansyah',
+            'body' => 'Doloribus sit autem cupiditate et eos aperiam nobis ullam laboriosam quo ipsum aut dignissimos rem natus. Possimus et nisi doloremque dolor molestias earum. Fugit quam aliquam dolor. Molestiae esse minima tenetur totam sit at molestias et.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/about', function () {
