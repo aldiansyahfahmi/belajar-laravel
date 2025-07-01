918b9b1bbe05c4e871db2a1a288e6d239142d91d
Aldiansyah Fahmi <52243491+aldiansyahfahmi@users.noreply.github.com>
2025-07-01 09:13:31 +0800

    eloquent orm & post model

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
-Route::get('/posts/{slug}', function ($slug) {
-    $post = Post::find($slug);
+Route::get('/posts/{id}', function ($id) {
+    $post = Post::find($id);
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
